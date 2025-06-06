e802c8bc6c84d2a5c8f8c7f039e51094f242fc01
Nicholas Bernstein <go69kih@mytum.de>
2025-06-06 22:10:04 +0200

    Update HTML, JS, and server files

---
+++ public/scripts.js
@@ -0,0 +1,22 @@
+const SUPABASE_URL = "https://fqsuegrrtmazhgjhvsmr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxc3VlZ3JydG1hemhnamh2c21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NDYxNDEsImV4cCI6MjA2MzMyMjE0MX0.nyUVXWRlFQcd8Nlp_3K4NXeUfCYROxIG6KJXL9-96Ls";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+async function fetchReviews() {
+    const { data, error } = await supabase
+        .from("reviews")
+        .select('id, company, websites, upvotes, comment')
+
+        //order the reviews table by upvotes and then created at
+        .order('upvotes', { ascending: false })
+        .order('created_at', { ascending: false });
+
+    if (error) {
+        console.error('Error fetching reviews:', error);
+        return [];
+    }
+
+    // the data is an array of review objects
+    return data;
+}   
--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-
-const app = express();
-const db = new sqlite3.Database('reviews.db');
